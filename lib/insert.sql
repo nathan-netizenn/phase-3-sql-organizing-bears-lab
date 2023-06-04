49d4da8306661361dafe89cf613e47a6899bf45e
nathan <nathanmageka6@gmail.com>
2023-06-04 22:58:38 +0300

    nathan

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', true),
+  ('Tabitha', 4, 'F', 'Gray', 'Calm', true),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Aggressive', true),
+  ('Melissa', 2, 'F', 'Golden', 'Energetic', true),
+  ('Grinch', 7, 'M', 'Green', 'Grouchy', true),
+  ('Wendy', 5, 'F', 'White', 'Sweet', true),
+  (NULL, NULL, 'F', NULL, NULL, NULL);
